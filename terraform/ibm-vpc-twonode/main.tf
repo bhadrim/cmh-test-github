05197b3e95fb0d7477a46decd9a9e6628454dbed
bhadrim <bhadrim@ca.ibm.com>
2021-02-05 17:43:07 -0500

    Update main.tf

--- terraform/ibm-vpc-twonode/main.tf
+++ terraform/ibm-vpc-twonode/main.tf
@@ -2,26 +2,31 @@ provider "ibm" {
   region = "${var.region}"
 }
 
+resource "random_integer" "key" {
+  min     = 1
+  max     = 50000
+}
+
 resource "ibm_is_vpc" "test_vpc" {
-  name = "test-vpc"
+  name = "test-vpc-${random_integer.key.result}"
   tags    = ["${concat(var.tags, module.camtags.tagslist)}"]
 }
 
 resource "ibm_is_subnet" "test_subnet" {
-  name            = "test-subnet"
+  name            = "test-subnet-${random_integer.key.result}"
   vpc             = "${ibm_is_vpc.test_vpc.id}"
   zone            = "${var.zone}"
   ipv4_cidr_block = "10.241.0.0/24"
 }
 
 resource "ibm_is_ssh_key" "test_sshkey" {
-  name       = "test-ssh"
+  name       = "test-ssh-${random_integer.key.result}"
   public_key = "${var.public_ssh_key}"
 }
 
 ## Web server VSI
 resource "ibm_is_instance" "web-server" {
-  name    = "web-server-vsi"
+  name    = "web-server-vsi-${random_integer.key.result}"
   image   = "${var.image_id}"
   profile = "${var.profile}"
 
@@ -39,32 +44,6 @@ resource "ibm_is_instance" "web-server" {
 resource "ibm_is_floating_ip" "test_floatingip" {
   name   = "test-fip"
   target = "${ibm_is_instance.web-server.primary_network_interface.0.id}"
-}
-
-## DB server VSI with additional data volume
-resource "ibm_is_instance" "db-server" {
-  name    = "db-server-vsi"
-  image   = "${var.image_id}"
-  profile = "${var.profile}"
-
-  primary_network_interface {
-    subnet = "${ibm_is_subnet.test_subnet.id}"
-  }
-
-  vpc     = "${ibm_is_vpc.test_vpc.id}"
-  volumes = ["${ibm_is_volume.test-volume.id}"]
-  zone    = "${var.zone}"
-  keys    = ["${ibm_is_ssh_key.test_sshkey.id}"]
-  tags    = ["${concat(var.tags, module.camtags.tagslist)}"]
-}
-
-resource "ibm_is_volume" "test-volume" {
-  name     = "test-volume"
-  profile  = "custom"
-  zone     = "${var.zone}"
-  iops     = 1000
-  capacity = 20
-  tags    = ["${concat(var.tags, module.camtags.tagslist)}"]
 }
 
 module "camtags" {
